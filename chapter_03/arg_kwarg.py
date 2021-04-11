046638ab8a6c4a55cf4e8cbb9c7abb4e3dfd8b16
Jess Ingrassellino <jessanickel@gmail.com>
2021-04-11 13:36:21 -0400

    Update arg_kwarg.py
    fixed typo in code comment

--- chapter_03/arg_kwarg.py
+++ chapter_03/arg_kwarg.py
@@ -1,6 +1,6 @@
 #args & kwargs
 def user_info(name, age=0, city="Tucson"):
-    '''This functoin prints name, age, and city from an argument provided to the function.'''
+    '''This function prints name, age, and city from an argument provided to the function.'''
 
     print("{} is {} years old, from {}".format(name, age, city))
 
